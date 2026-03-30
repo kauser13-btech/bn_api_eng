ebf6f33772dd13eaa7178e19183e99470648c3c8
Rabin <bdrabin@gmail.com>
2026-03-30 09:44:16 +0600

    update

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -1342,6 +1342,7 @@
                 }
             });
         }
+        
         jQuery(document).ready(function($) {
             $('#divisions').change(function() {
                 divisions();
